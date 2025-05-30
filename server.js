73169ad388dd1cc3379496f2616fa23cf4931844
Jonatan Troche <trochealmediajonatan@gmail.com>
2025-05-30 02:41:41 -0400

    create account

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities");
 const session = require("express-session")
 const pool = require('./database/')
-
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -39,6 +39,9 @@ app.use(function(req, res, next){
   next()
 })
 
+//Body-parser 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
