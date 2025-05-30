ba10a45790644cd098a9bee0878675f44a7457df
Jonatan Troche <trochealmediajonatan@gmail.com>
2025-05-30 00:12:37 -0400

    register view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,7 +4,8 @@ const router = new express.Router()
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
 
-router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/login", utilities.handleErrors(accountController.buildLogin));-
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 
 module.exports = router;
